8b1b6818a87504a92d2c282e5512af1af6845caa
gachimeni1411 <hasegawa14112535@gmail.com>
2022-07-23 00:15:52 +0900

    first

---
+++ Assets/CamCtrl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class CamCtrl : MonoBehaviour
+{
+    private Camera cam;
+    private Vector3 startPos;
+    private Vector3 startAngle;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        if (cam == null)
+        {
+            return;
+        }
+ 
+        float sensitiveMove = 0.8f;
+        float sensitiveRotate = 5.0f;
+        float sensitiveZoom = 10.0f;
+   
+        
+            // rotate camera
+            float rotateX = Input.GetAxis("Mouse X") * sensitiveRotate;
+            float rotateY = Input.GetAxis("Mouse Y") * sensitiveRotate;
+            cam.transform.Rotate(rotateY, rotateX, 0.0f);
+        
+ 
+        // zoom camera
+        float moveZ = Input.GetAxis("Mouse ScrollWheel") * sensitiveZoom;
+        cam.transform.position += cam.transform.forward * moveZ;
+    }
+}
